d9b8e1d1bcba5effa2dcdc681b83875dd74a951a
godcong <jumbycc@163.com>
2024-06-19 01:24:11 +0800

    feat(context): add context

---
+++ context/context_test.go
@@ -0,0 +1,42 @@
+package context
+
+// unused check
+var (
+	_ = TODO
+	_ = WithDeadlineCause
+	_ = WithCancelCause
+	_ = WithTimeoutCause
+	_ = WithValue
+	_ = WithCancel
+	_ = Cause
+	_ = AfterFunc
+	_ = WithoutCancel
+	_ = WithTimeout
+	_ = WithDeadline
+	_ = NewRowLock
+	_ = FromRowLock
+	_ = NewCreatedBy
+	_ = FromCreatedBy
+	_ = NewLogger
+	_ = FromLogger
+	_ = Alias
+	_ = Background
+	_ = NewTraceID
+	_ = FromTrans
+	_ = FromTraceID
+	_ = NewTrans
+	_ = NewUserID
+	_ = FromUserID
+	_ = NewUserToken
+	_ = FromUserToken
+	_ = NewIsRootUser
+	_ = FromIsRootUser
+	_ = NewUserCache
+	_ = FromUserCache
+	_ = NewTag
+	_ = FromTag
+	_ = NewStack
+	_ = FromStack
+	_ = NewDB
+	_ = FromDB
+)
