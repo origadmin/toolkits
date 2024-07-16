c65b826e21415c4de08c427a53aa1891b78bcb1d
godcong <jumbycc@163.com>
2024-07-16 16:11:20 +0800

    feat(metrics): Update developing metrics

--- metrics/config.go
+++ metrics/config.go
@@ -1,7 +1,6 @@
 package metrics
 
 import (
-	"net/http"
 	"time"
 )
 
@@ -14,24 +13,24 @@ const (
 
 // Config represents the configuration for the Service server.
 type Config struct {
-	Enable          bool                // Enable specifies if the server is enabled.
-	ListenPort      int                 // ListenPort is the port on which the server listens.
-	ReadTimeout     time.Duration       // ReadTimeout is the duration for reading the entire request.
-	WriteTimeout    time.Duration       // WriteTimeout is the duration for writing the entire response.
-	MaxHeaderBytes  int                 // MaxHeaderBytes is the maximum number of bytes the server will read parsing the request header.
-	Handler         http.Handler        // Handler is the handler to invoke for incoming requests.
-	DurationBuckets []float64           // DurationBuckets are the buckets for histogram observations based on time.
-	SizeBuckets     []float64           // SizeBuckets are the buckets for histogram observations based on size.
-	Namespace       string              // Namespace is the identifier for metrics in the namespace.
-	SubSystem       string              // SubSystem is the subsystem identifier for metrics.
-	Buckets         []float64           // Buckets are the predefined histogram buckets.
-	Objectives      map[float64]float64 // Objectives contains quantile ranks and their error allowances.
-	DefaultCollect  bool                // DefaultCollect specifies if metrics should be collected by default.
-	BasicUserName   string              // BasicUserName is the username for basic authentication.
-	BasicPassword   string              // BasicPassword is the password for basic authentication.
-	LogMethod       map[string]struct{} // LogMethod is the set of methods to log.
-	LogAPI          map[string]struct{} // LogAPI is the set of APIs to log.
-	Application     string              // Application is the name of the application.
+	Enable          bool                    // Enable specifies if the server is enabled.
+	ListenPort      int                     // ListenPort is the port on which the server listens.
+	ReadTimeout     time.Duration           // ReadTimeout is the duration for reading the entire request.
+	WriteTimeout    time.Duration           // WriteTimeout is the duration for writing the entire response.
+	MaxHeaderBytes  int                     // MaxHeaderBytes is the maximum number of bytes the server will read parsing the request header.
+	DurationBuckets []float64               // DurationBuckets are the buckets for histogram observations based on time.
+	SizeBuckets     []float64               // SizeBuckets are the buckets for histogram observations based on size.
+	Namespace       string                  // Namespace is the identifier for metrics in the namespace.
+	SubSystem       string                  // SubSystem is the subsystem identifier for metrics.
+	Buckets         []float64               // Buckets are the predefined histogram buckets.
+	Objectives      map[float64]float64     // Objectives contains quantile ranks and their error allowances.
+	DefaultCollect  bool                    // DefaultCollect specifies if metrics should be collected by default.
+	BasicUserName   string                  // BasicUserName is the username for basic authentication.
+	BasicPassword   string                  // BasicPassword is the password for basic authentication.
+	MetricLabels    map[MetricType][]string // MetricLabels is the set of labels for each metric type.
+	LogMethod       map[string]struct{}     // LogMethod is the set of methods to log.
+	LogAPI          map[string]struct{}     // LogAPI is the set of APIs to log.
+	Application     string                  // Application is the name of the application.
 }
 
 // DefaultConfig returns the default configuration for the Service server.
@@ -44,5 +43,9 @@ func DefaultConfig() *Config {
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
+		Namespace:      defaultNamespace,
+		SubSystem:      defaultSubSystem,
+		Buckets:        []float64{},
+		Objectives:     map[float64]float64{0.9: 0.01, 0.95: 0.005, 0.99: 0.001},
 	}
 }
--- metrics/metrics.go
+++ metrics/metrics.go
@@ -3,14 +3,98 @@
 // Package metrics provides the metrics recorder
 package metrics
 
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type MetricType string
+
+const (
+	MetricRequestTotal     MetricType = "request_total"
+	MetricSendBytes        MetricType = "send_bytes"
+	MetricRecvBytes        MetricType = "recv_bytes"
+	MetricException        MetricType = "exception"
+	MetricEvent            MetricType = "event"
+	MetricSiteEvent        MetricType = "site_event"
+	MetricHistogramLatency MetricType = "histogram_latency"
+	MetricSummaryLatency   MetricType = "summary_latency"
+	MetricGaugeState       MetricType = "gauge_state"
+)
+
+func (m MetricType) String() string {
+	return string(m)
+}
+
+type dummyMetrics struct{}
+
 // Metrics is the metrics interface for metrics
 type Metrics interface {
-	// Enabled returns whether metrics is enabled
-	Enabled() bool
-
-	// Observe records a metric using the provided Reporter
+	Enabled() bool // Enabled returns whether metrics is enabled
 	Observe(reporter Reporter)
-
-	// Recorder returns the Recorder for this Metrics instance
-	Recorder() Recorder
+	Log(api, method, code string, sendBytes, recvBytes, latency float64)
+	RequestLog(module, api, method, code string)
+	SendBytesLog(module, api, method, code string, length float64)
+	RecvBytesLog(module, api, method, code string, length float64)
+	HistogramLatencyLog(module, api, method string, latency float64)
+	SummaryLatencyLog(module, api, method string, latency float64)
+	ExceptionLog(module, exception string)
+	EventLog(module, event string)
+	SiteEventLog(module, event, site string)
+	StateLog(module, state string, value float64)
 }
+
+var metricLabels = map[MetricType][]string{
+	MetricRequestTotal:     {"app", "module", "api", "method", "code"},
+	MetricSendBytes:        {"app", "module", "api", "method", "code"},
+	MetricRecvBytes:        {"app", "module", "api", "method", "code"},
+	MetricException:        {"app", "module", "exception"},
+	MetricEvent:            {"app", "module", "event"},
+	MetricSiteEvent:        {"app", "module", "event", "site"},
+	MetricHistogramLatency: {"app", "module", "api", "method"},
+	MetricSummaryLatency:   {"app", "module", "api", "method"},
+	MetricGaugeState:       {"app", "module", "state"},
+}
+
+// MetricTypeLabels returns the labels for the given metric type
+func MetricTypeLabels(metricType MetricType) []string {
+	if v, ok := metricLabels[metricType]; ok {
+		return v
+	}
+	return []string{}
+}
+
+// MetricLabels returns the labels for the given metric type
+func MetricLabels() map[MetricType][]string {
+	return metricLabels
+}
+
+func (d dummyMetrics) Enabled() bool {
+	return false
+}
+func (d dummyMetrics) Observe(reporter Reporter) {}
+
+func (d dummyMetrics) Log(api, method, code string, sendBytes, recvBytes, latency float64) {}
+
+func (d dummyMetrics) RequestLog(module, api, method, code string) {}
+
+func (d dummyMetrics) SendBytesLog(module, api, method, code string, byte float64) {}
+
+func (d dummyMetrics) RecvBytesLog(module, api, method, code string, byte float64) {}
+
+func (d dummyMetrics) HistogramLatencyLog(module, api, method string, latency float64) {}
+
+func (d dummyMetrics) SummaryLatencyLog(module, api, method string, latency float64) {}
+
+func (d dummyMetrics) ExceptionLog(module, exception string) {}
+
+func (d dummyMetrics) EventLog(module, event string) {}
+
+func (d dummyMetrics) SiteEventLog(module, event, site string) {}
+
+func (d dummyMetrics) StateLog(module, state string, value float64) {}
+
+func (d dummyMetrics) ResetCounter() {}
+
+func (d dummyMetrics) RegisterCustomCollector(c prometheus.Collector) {}
+
+var DummyMetrics Metrics = dummyMetrics{}
