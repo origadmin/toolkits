eb5486d0eb7b668fef642c6627aec989dad42210
godcong <jumbycc@163.com>
2024-07-16 19:17:00 +0800

    refactor(setting): generalize setting type to use any type parameter
    Update the Setting type to be a generic function type, allowing it to accept
    any type parameter S. This change simplifies the Apply function signature by
    removing the redundant type parameter and enhances the flexibility of the
    setting package.
    
    BREAKING CHANGE: This change may affect consumers of the setting package who
    rely on the previous non-generic definition of the Setting type.

--- setting/setting.go
+++ setting/setting.go
@@ -1,7 +1,9 @@
 package setting
 
+type Setting[S any] func(*S)
+
 // Apply is apply settings
-func Apply[T func(*S), S any](d S, settings ...T) *S {
+func Apply[T Setting[S], S any](d S, settings ...T) *S {
 	val := &d
 	for _, setting := range settings {
 		setting(val)
