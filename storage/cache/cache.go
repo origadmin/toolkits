1be8dbfd53cb4c6d728f376907a8f0fcab5ff4bc
godcong <jumbycc@163.com>
2024-09-19 14:13:14 +0800

    feat(storage/cache): add Cache interface with basic operations
    Implement a preliminary Cache interface containing basic operations such as Get, Set, and Delete.
    The interface uses strings as value types, facilitating easy conversion to []byte for zero-copy optimizations in go1.22+. Additionally, the interface includes error handling specific to cache
    operations, introducing custom error types for cache-related errors.
    
    BREAKING CHANGE: New cache interface and error handling may affect existing storage patterns.

---
+++ storage/cache/cache.go
@@ -0,0 +1,32 @@
+package cache
+
+import (
+	"time"
+
+	"github.com/origadmin/toolkits/context"
+)
+
+// Cache is the interface that wraps the basic Get, Set, and Delete methods.
+// It uses string as the value type, allowing for flexible conversion to []byte
+// and zero-copy resource optimization after go1.22.
+type Cache interface {
+	// Get retrieves the value associated with the given key.
+	// It returns the cached value and an error if the key is not found.
+	Get(ctx context.Context, key string) (string, error)
+
+	// GetAndDelete retrieves the value associated with the given key and deletes it.
+	// It returns the cached value and an error if the key is not found.
+	GetAndDelete(ctx context.Context, key string) (string, error)
+
+	// Exists checks if a value exists for the given key.
+	// It returns an error if the key is not found.
+	Exists(ctx context.Context, key string) error
+
+	// Set sets the value for the given key.
+	// It returns an error if the operation fails.
+	Set(ctx context.Context, key string, value string, expiration ...time.Duration) error
+
+	// Delete deletes the value associated with the given key.
+	// It returns an error if the operation fails.
+	Delete(ctx context.Context, key string) error
+}
---
+++ storage/cache/error.go
@@ -0,0 +1,50 @@
+package cache
+
+import (
+	"errors"
+)
+
+type cacheError struct {
+	msg string
+	err error
+}
+
+func (c *cacheError) Error() string {
+	if c.err == nil {
+		return c.msg
+	}
+	return c.msg + ": " + c.err.Error()
+}
+
+func (c *cacheError) Unwrap() error {
+	return c.err
+}
+
+func (c *cacheError) Is(err error) bool {
+	if err == nil {
+		return c == nil
+	}
+
+	var cacheError *cacheError
+	ok := errors.As(err, &cacheError)
+	if !ok {
+		return false
+	}
+	if cacheError.msg != c.msg {
+		return errors.Is(err, c.err)
+	}
+	return ok
+}
+
+var (
+	ErrCacheClosed   error = &cacheError{msg: "cache closed"}
+	ErrCacheNotFound error = &cacheError{msg: "cache not found"}
+)
+
+func NewError(msg string) error {
+	return &cacheError{msg: msg}
+}
+
+func NewErrorWith(msg string, err error) error {
+	return &cacheError{msg: msg, err: err}
+}
