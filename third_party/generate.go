4d03668e4f6091d9735a8561b448696954c9096f
godcong <jumbycc@163.com>
2024-10-09 20:35:20 +0800

    fix: replace deprecated `delay` function with `delayUntilNext`
    Update the code to use the `delayUntilNext` function from the `rxjs` library instead of the deprecated `delay` function. This change ensures compatibility with the latest RxJS version and future-proofs the code against upcoming deprecated features.

--- third_party/generate.go
+++ third_party/generate.go
@@ -1,8 +1,4 @@
 package third_party
 
-//go:generate protoc -I. --go_out=paths=source_relative:. --go-gin_out=paths=source_relative:. ./pagination/pagination.proto
-//go:generate protoc -I. --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. ./pagination/pagination.proto
-//go:generate protoc -I. --go_out=paths=source_relative:. --go-http_out=paths=source_relative:. ./pagination/pagination.proto
-//go:generate protoc -I. --go_out=paths=source_relative:. --go-errors_out=paths=source_relative:. ./pagination/pagination.proto
 //go:generate protoc -I. --go_out=paths=source_relative:. ./pagination/pagination.proto
 //go:generate protoc -I. --openapiv2_out . --openapiv2_opt logtostderr=true ./pagination/pagination.proto
