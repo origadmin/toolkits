e4f62d2578fe845affa6024f979a80669bc18fd1
godcong <jumbycc@163.com>
2024-10-29 18:52:52 +0800

    feat(runtime): add CORS middleware for Fiber web framework
    - Implement CORS middleware configuration for Fiber
    - Create new Fiber handler with CORS configuration
    - Add go.mod and go.sum files for Fiber runtime dependencies

---
+++ runtime/web/fiber/go.mod
@@ -0,0 +1,25 @@
+module github.com/origadmin/toolkits/runtime/web/fiber
+
+go 1.23.1
+
+replace github.com/origadmin/toolkits => ../../../
+
+require (
+	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/origadmin/toolkits v0.0.0-00010101000000-000000000000
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+)
---
+++ runtime/web/fiber/middlewares/cors/cors.go
@@ -0,0 +1,54 @@
+package cors
+
+import (
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+
+	"github.com/origadmin/toolkits/middlewares"
+)
+
+const (
+	corsOptionMethod              string = "OPTIONS"
+	corsAllowOriginHeader         string = "Access-Control-Allow-Origin"
+	corsExposeHeadersHeader       string = "Access-Control-Expose-Headers"
+	corsMaxAgeHeader              string = "Access-Control-Max-Age"
+	corsAllowMethodsHeader        string = "Access-Control-Allow-Methods"
+	corsAllowHeadersHeader        string = "Access-Control-Allow-Headers"
+	corsAllowCredentialsHeader    string = "Access-Control-Allow-Credentials"
+	corsAllowPrivateNetworkHeader string = "Access-Control-Allow-Private-Network"
+	corsRequestMethodHeader       string = "Access-Control-Request-Method"
+	corsRequestHeadersHeader      string = "Access-Control-Request-Headers"
+	corsRequestPrivateNetwork     string = "Access-Control-Request-Private-Network"
+	corsOriginHeader              string = "Origin"
+	corsVaryHeader                string = "Vary"
+	corsMatchAll                  string = "*"
+)
+
+func WithCors(cfg *middlewares.CorsConfig) fiber.Handler {
+	if !cfg.Enabled {
+		return func(ctx *fiber.Ctx) error {
+			return ctx.Next()
+		}
+	}
+
+	allOrigins := corsMatchAll
+	if len(cfg.AllowOrigins) > 0 && cfg.AllowOrigins[0] != corsMatchAll {
+		allOrigins = strings.Join(cfg.AllowOrigins, ",")
+	}
+
+	//if allOrigins && cfg.AllowCredentials {
+	//	panic("[CORS] Insecure setup, 'AllowCredentials' is set to true, and 'AllowOrigins' is set to a wildcard.")
+	//}
+
+	return cors.New(cors.Config{
+		AllowOriginsFunc: nil,
+		AllowOrigins:     allOrigins,
+		AllowMethods:     strings.Join(cfg.AllowMethods, ","),
+		AllowHeaders:     strings.Join(cfg.AllowHeaders, ","),
+		ExposeHeaders:    strings.Join(cfg.ExposeHeaders, ","),
+		AllowCredentials: cfg.AllowCredentials,
+		MaxAge:           int(cfg.MaxAge.GetSeconds()),
+	})
+}
