e3c5e816a6a23736d3dc96c98e3943864261e90d
godcong <jumbycc@163.com>
2024-10-29 23:26:49 +0800

    build: update protobuf generation commands for middlewares
    - Change import path from ./third_party/middlewares to ./middlewares
    - Update generate.go to reflect the new directory structure

--- generate.go
+++ generate.go
@@ -2,8 +2,8 @@ package toolkits
 
 //go:generate protoc -I. -I./third_party --go_out=paths=source_relative:. ./third_party/errors/rpcerr/*.proto
 
-//go:generate protoc -I. -I./third_party --go_out=paths=source_relative:. ./third_party/middlewares/*.proto
-//go:generate protoc -I. -I./third_party --validate_out=paths=source_relative,lang=go:. ./third_party/middlewares/*.proto
+//go:generate protoc -I. -I./third_party --go_out=paths=source_relative:. ./middlewares/*.proto
+//go:generate protoc -I. -I./third_party --validate_out=paths=source_relative,lang=go:. ./middlewares/*.proto
 
 //go:generate protoc -I. -I./third_party --go_out=paths=source_relative:. ./runtime/config/*.proto
 //go:generate protoc -I. -I./third_party --validate_out=paths=source_relative,lang=go:. ./runtime/config/*.proto
