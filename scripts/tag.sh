1b5e13e2758c0c5eb79201abaa36b9a47df947fe
godcong <jumbycc@163.com>
2024-11-15 18:39:16 +0800

    feat(scripts): improve tag.sh script and related functionality
    - Update tag.sh to handle stashed changes and branch switching
    - Refactor test.sh and upm.sh scripts for better readability and maintainability
    - Improve error handling and add more informative output in all scripts

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -13,20 +13,20 @@ check_for_go_mod() {
     local dir="$1"
     local go_mod_name="go.mod"
     local updated=1 # Assume not updated by default
-
+    
     # Change to the directory
     cd "$dir" || return 1
-
+    
     # Check if the go.mod file exists in the directory
     if [ -f "$go_mod_name" ]; then
         echo "Find dependencies go.mod in directories:"
         echo " ->DIR: $dir"
         updated=0
     fi
-
+    
     # Return to the original working directory for the next iteration
     cd "$ORIGINAL_DIR" || return 1
-
+    
     # Return the update status
     return $updated
 }
@@ -37,22 +37,22 @@ function_checks() {
         echo "Error: get_latest_tag function is not defined"
         exit 1
     fi
-
+    
     if ! declare -f get_matching_tags >/dev/null; then
         echo "Error: get_matching_tags function is not defined"
         exit 1
     fi
-
+    
     if ! declare -f get_head_version_tag >/dev/null; then
         echo "Error: get_head_version_tag function is not defined"
         exit 1
     fi
-
+    
     if ! declare -f get_next_module_version >/dev/null; then
         echo "Error: get_next_module_version function is not defined"
         exit 1
     fi
-
+    
     if ! declare -f get_latest_module_tag >/dev/null; then
         echo "Error: get_latest_module_tag function is not defined"
         exit 1
@@ -65,11 +65,11 @@ handle_go_mod_directory() {
     local module="main"
     local module_name
     module_name="$(echo "$dir" | sed "s/^.\///")" # Drop the beginning './'
-
+    
     if [ "$module_name" != "." ]; then
         module="$module_name"
     fi
-
+    
     if check_for_go_mod "$dir"; then
         local HEAD_TAG
         local LATEST_TAG
@@ -85,15 +85,15 @@ handle_go_mod_directory() {
         echo " ->LATEST_TAG: $LATEST_TAG"
         NEXT_TAG=$(get_next_module_version "$module_name")
         echo " ->NEXT_TAG: $NEXT_TAG"
-
+        
         if [ -z "$HEAD_TAG" ]; then
             echo "Creating new tag: $NEXT_TAG"
             create_new_tag "$NEXT_TAG"
         fi
-
+        
         echo ""
     fi
-
+    
 }
 
 # Define a function to traverse directories and apply the handle_go_mod_directory  function
@@ -101,15 +101,24 @@ add_go_mod_tag() {
     echo "Checking for go.mod files..."
     function_checks
     local module_name="$1"
-
+    
     echo ""
     echo "COMMIT_HASH: $(get_current_commit_hash "$module_name")"
     echo ""
 
+    local current_branch="main"
+    current_branch=$(git branch --show-current)
+		echo "Current branch: $current_branch"
+
+		# Save all local changes to the Git stash
+		echo "Saving all local changes and change to the main branch..."
+    git stash save --include-untracked
+    git checkout -B "main" "original/main"
+
     # If a module_name is specified, process only that directory
     if [ "$module_name" == "." ]; then
         handle_go_mod_directory "."
-    elif [ -n "$module_name" ]; then
+        elif [ -n "$module_name" ]; then
         handle_go_mod_directory "$module_name"
     else
         # Skip the root directory ('.')
@@ -120,6 +129,10 @@ add_go_mod_tag() {
             handle_go_mod_directory "$dir"
         done
     fi
+
+    # Restore the original branch and pop the stash
+    git checkout "$current_branch"
+    git stash pop
 }
 
 add_go_mod_tag "$1"
