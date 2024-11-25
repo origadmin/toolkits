df1e79ebe24e95c3863ad5f292389e27093c3b21
godcong <jumbycc@163.com>
2024-11-25 15:53:07 +0800

    feat(toolkits): add new packages and functionalities
    - Add pagination package with CoalesceMax and OrdersBy functions
    - Add mail configuration protobuf file
    - Update runtime config package to use new Customize type

--- runtime/config/config.go
+++ runtime/config/config.go
@@ -12,20 +12,22 @@ import (
 const Type = "config"
 
 type Setting struct {
-	Options []Option
-	Custom  *configv1.Custom
+	Options   []Option
+	Customize *configv1.Customize
 }
 
 type SettingFunc = func(s *Setting)
 
+// WithOptions sets the options field of the Setting struct.
 func WithOptions(options ...Option) SettingFunc {
 	return func(s *Setting) {
 		s.Options = options
 	}
 }
 
-func WithCustom(custom *configv1.Custom) SettingFunc {
+// WithCustomize sets the customize field of the Setting struct.
+func WithCustomize(custom *configv1.Customize) SettingFunc {
 	return func(s *Setting) {
-		s.Custom = custom
+		s.Customize = custom
 	}
 }
