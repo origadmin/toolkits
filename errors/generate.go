785ca6bee2d4fff8215e93631f0a3b01654c2108
godcong <jumbycc@163.com>
2024-11-26 17:24:06 +0800

    refactor(generate): move generate.go to errors package
    - Move generate.go from toolkits directory to errors directory
    - Update package comment to reflect new location

--- errors/generate.go
+++ errors/generate.go
@@ -2,7 +2,8 @@
  * Copyright (c) 2024 OrigAdmin. All rights reserved.
  */
 
-package toolkits
+// Package errors provides the error functions
+package errors
 
 //go:generate buf dep update
 //go:generate buf build
