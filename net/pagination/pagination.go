daee2e70c56a0cd97ea90fb1c9c519dfde35945c
godcong <jumbycc@163.com>
2024-11-27 16:02:26 +0800

    refactor(pagination): remove protobuf imports and update interfaces
    - Remove unused protobuf imports for anypb and fieldmaskpb
    - Update Requester interface:
    - Change GetFieldMask() to return []string instead of *fieldmaskpb.FieldMask
    - Update Responder interface:
      - Change GetData() to return any instead of *anypb.Any
      - Change GetExtra() to return any instead of *anypb.Any

--- net/pagination/pagination.go
+++ net/pagination/pagination.go
@@ -5,11 +5,6 @@
 // Package pagination implements the functions, types, and interfaces for the module.
 package pagination
 
-import (
-	"google.golang.org/protobuf/types/known/anypb"
-	"google.golang.org/protobuf/types/known/fieldmaskpb"
-)
-
 // Requester is the request interface for the module.
 type Requester interface {
 	GetCurrent() int32
@@ -18,16 +13,16 @@ type Requester interface {
 	GetOnlyCount() bool
 	GetNoPaging() bool
 	GetOrderBy() []string
-	GetFieldMask() *fieldmaskpb.FieldMask
+	GetFieldMask() []string
 }
 
 // Responder is the response interface for the module.
 type Responder interface {
 	GetSuccess() bool
 	GetTotal() int32
-	GetData() *anypb.Any
+	GetData() any
 	GetCurrent() int32
 	GetPageSize() int32
 	GetNextPageToken() string
-	GetExtra() *anypb.Any
+	GetExtra() any
 }
