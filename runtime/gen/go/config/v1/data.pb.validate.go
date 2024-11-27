c8393ae4d8f413d943f75c93649fe129de1d7b2f
godcong <jumbycc@163.com>
2024-11-28 02:22:34 +0800

    chore(proto): add gnostic to buf deps

--- runtime/gen/go/config/v1/data.pb.validate.go
+++ runtime/gen/go/config/v1/data.pb.validate.go
@@ -886,6 +886,114 @@ var _ interface {
 	ErrorName() string
 } = Data_MemoryValidationError{}
 
+// Validate checks the field values on Data_BadgerDS with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *Data_BadgerDS) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on Data_BadgerDS with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in Data_BadgerDSMultiError, or
+// nil if none found.
+func (m *Data_BadgerDS) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *Data_BadgerDS) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Path
+
+	// no validation rules for SyncWrites
+
+	// no validation rules for ValueLogFileSize
+
+	// no validation rules for LogLevel
+
+	if len(errors) > 0 {
+		return Data_BadgerDSMultiError(errors)
+	}
+
+	return nil
+}
+
+// Data_BadgerDSMultiError is an error wrapping multiple validation errors
+// returned by Data_BadgerDS.ValidateAll() if the designated constraints
+// aren't met.
+type Data_BadgerDSMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m Data_BadgerDSMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m Data_BadgerDSMultiError) AllErrors() []error { return m }
+
+// Data_BadgerDSValidationError is the validation error returned by
+// Data_BadgerDS.Validate if the designated constraints aren't met.
+type Data_BadgerDSValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e Data_BadgerDSValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e Data_BadgerDSValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e Data_BadgerDSValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e Data_BadgerDSValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e Data_BadgerDSValidationError) ErrorName() string { return "Data_BadgerDSValidationError" }
+
+// Error satisfies the builtin error interface
+func (e Data_BadgerDSValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sData_BadgerDS.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = Data_BadgerDSValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = Data_BadgerDSValidationError{}
+
 // Validate checks the field values on Data_File with the rules defined in the
 // proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
@@ -1268,6 +1376,35 @@ func (m *Data_Storage) validate(all bool) error {
 	}
 
 	if all {
+		switch v := interface{}(m.GetBadger()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, Data_StorageValidationError{
+					field:  "Badger",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, Data_StorageValidationError{
+					field:  "Badger",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetBadger()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return Data_StorageValidationError{
+				field:  "Badger",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if all {
 		switch v := interface{}(m.GetMongo()).(type) {
 		case interface{ ValidateAll() error }:
 			if err := v.ValidateAll(); err != nil {
@@ -1427,35 +1564,6 @@ func (m *Data_Cache) validate(all bool) error {
 	// no validation rules for Driver
 
 	if all {
-		switch v := interface{}(m.GetRedis()).(type) {
-		case interface{ ValidateAll() error }:
-			if err := v.ValidateAll(); err != nil {
-				errors = append(errors, Data_CacheValidationError{
-					field:  "Redis",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		case interface{ Validate() error }:
-			if err := v.Validate(); err != nil {
-				errors = append(errors, Data_CacheValidationError{
-					field:  "Redis",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		}
-	} else if v, ok := interface{}(m.GetRedis()).(interface{ Validate() error }); ok {
-		if err := v.Validate(); err != nil {
-			return Data_CacheValidationError{
-				field:  "Redis",
-				reason: "embedded message failed validation",
-				cause:  err,
-			}
-		}
-	}
-
-	if all {
 		switch v := interface{}(m.GetMemcached()).(type) {
 		case interface{ ValidateAll() error }:
 			if err := v.ValidateAll(); err != nil {
@@ -1507,6 +1615,64 @@ func (m *Data_Cache) validate(all bool) error {
 		if err := v.Validate(); err != nil {
 			return Data_CacheValidationError{
 				field:  "Memory",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if all {
+		switch v := interface{}(m.GetRedis()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, Data_CacheValidationError{
+					field:  "Redis",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, Data_CacheValidationError{
+					field:  "Redis",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetRedis()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return Data_CacheValidationError{
+				field:  "Redis",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if all {
+		switch v := interface{}(m.GetBadger()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, Data_CacheValidationError{
+					field:  "Badger",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, Data_CacheValidationError{
+					field:  "Badger",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetBadger()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return Data_CacheValidationError{
+				field:  "Badger",
 				reason: "embedded message failed validation",
 				cause:  err,
 			}
