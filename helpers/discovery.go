395b979d8ffb7325aff9cb054d7a7edfba41669f
godcong <jumbycc@163.com>
2024-12-04 19:58:53 +0800

    feat(net): add service endpoint generation and host address retrieval
    - Add ServiceName function as a non-deprecated alternative to ServiceDiscoveryName
    - Implement ServiceEndpoint function to generate service endpoint URL
    - Add net/host.go file with HostAddr function to retrieve host IP address
    - Update imports in discovery.go to include net package

--- helpers/discovery.go
+++ helpers/discovery.go
@@ -6,6 +6,7 @@
 package helpers
 
 import (
+	"net"
 	"net/netip"
 	"strconv"
 	"strings"
@@ -15,10 +16,18 @@ const (
 	defaultDiscoveryPrefix = "discovery:///"
 )
 
+// ServiceDiscoveryName ...
+// Deprecated: use ServiceName
 func ServiceDiscoveryName(serviceName string) string {
 	return defaultDiscoveryPrefix + serviceName
 }
 
+func ServiceName(serviceName string) string {
+	return defaultDiscoveryPrefix + serviceName
+}
+
+// ServiceDiscoveryEndpoint ...
+// Deprecated: use ServiceEndpoint
 func ServiceDiscoveryEndpoint(endpoint, scheme, host, addr string) string {
 	naip, _ := netip.ParseAddrPort(addr)
 	if endpoint == "" {
@@ -40,4 +49,66 @@ func ServiceDiscoveryEndpoint(endpoint, scheme, host, addr string) string {
 		}
 	}
 	return endpoint
+}
+
+func ServiceEndpoint(scheme, host, hostPort string) string {
+	_, port, err := net.SplitHostPort(hostPort)
+	if err != nil && host == "" {
+		return hostPort
+	}
+	if len(host) > 0 && (host != "0.0.0.0" && host != "[::]" && host != "::") {
+		return schemeHost(scheme, net.JoinHostPort(host, port))
+	}
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return schemeHost(scheme, hostPort)
+	}
+	minIndex := int(^uint(0) >> 1)
+	ips := make([]net.IP, 0)
+	for _, iface := range ifaces {
+		if (iface.Flags & net.FlagUp) == 0 {
+			continue
+		}
+		if iface.Index >= minIndex && len(ips) != 0 {
+			continue
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			continue
+		}
+		for i, rawAddr := range addrs {
+			var ip net.IP
+			switch addr := rawAddr.(type) {
+			case *net.IPAddr:
+				ip = addr.IP
+			case *net.IPNet:
+				ip = addr.IP
+			default:
+				continue
+			}
+			if isValidIP(ip.String()) {
+				minIndex = iface.Index
+				if i == 0 {
+					ips = make([]net.IP, 0, 1)
+				}
+				ips = append(ips, ip)
+				if ip.To4() != nil {
+					break
+				}
+			}
+		}
+	}
+	if len(ips) != 0 {
+		return schemeHost(scheme, net.JoinHostPort(ips[len(ips)-1].String(), port))
+	}
+	return schemeHost(scheme, hostPort)
+}
+
+func schemeHost(scheme, host string) string {
+	return scheme + "://" + host
+}
+
+func isValidIP(addr string) bool {
+	ip := net.ParseIP(addr)
+	return ip.IsGlobalUnicast() && !ip.IsInterfaceLocalMulticast()
 }
