fe11baafa7e96ba87867c66214e40bd5f1570450
godcong <jumbycc@163.com>
2024-12-10 02:16:47 +0800

    refactor(security): replace ContextType with TokenType

--- security/security.go
+++ security/security.go
@@ -5,4 +5,13 @@
 // Package security implements the functions, types, and interfaces for the module.
 package security
 
-type ContextType string
+type TokenType int
+
+const (
+	ContextTypeContext TokenType = iota
+	ContextTypeHeader
+)
+
+const (
+	HeaderAuthorize = "Authorization"
+)
