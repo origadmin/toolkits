437d40434d3401dc9ff20798aa3416abffaf0642
godcong <jumbycc@163.com>
2024-12-09 16:52:50 +0800

    feat(security): implement interfaces for authentication and authorization
    - Add Authenticator interface for token authentication and management
    - Add Policy interface for defining access control policies
    - Add Authorizer interface for checking authorization
    - Implement Claims interface for security claims
    - Remove unnecessary files and refactor existing code

---
+++ security/authn.go
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2024 OrigAdmin. All rights reserved.
+ */
+
+// Package security implements the functions, types, and interfaces for the module.
+package security
+
+import (
+	"context"
+)
+
+// Authenticator interface
+type Authenticator interface {
+	// AuthenticateToken returns a nil error and the AuthClaims info (if available).
+	AuthenticateToken(string) (Claims, error)
+	// AuthenticateTokenContext returns a nil error and the AuthClaims info (if available).
+	// if the subject is authenticated or a non-nil error with an appropriate error cause otherwise.
+	AuthenticateTokenContext(context.Context, ContextType) (Claims, error)
+	// Authenticate validates if a token is valid.
+	Authenticate(context.Context, string) (bool, error)
+	// AuthenticateContext validates if a token is valid.
+	AuthenticateContext(context.Context, ContextType, string) (bool, error)
+
+	// CreateToken inject user claims into token string.
+	CreateToken(Claims) (string, error)
+	// CreateTokenContext inject user claims into context.
+	CreateTokenContext(context.Context, ContextType, Claims) (context.Context, error)
+	// DestroyToken invalidate a token by removing it from the token store.
+	DestroyToken(context.Context, string) error
+	// DestroyTokenContext invalidate a token by removing it from the token store.
+	DestroyTokenContext(context.Context, ContextType, string) error
+
+	// Close Cleans up the authenticator.
+	Close()
+}
--- security/serialize.go
+++ security/serialize.go
@@ -2,13 +2,13 @@
  * Copyright (c) 2024 OrigAdmin. All rights reserved.
  */
 
+// Package security provides interfaces and types for security-related operations
 package security
 
-import (
-	"time"
-)
-
-type TokenSerializer interface {
-	Generate(subject string, expires ...time.Duration) Token
-	Parse(token string) (Token, error)
+// Serializer is an interface that defines the methods for a serializer
+type Serializer interface {
+	// Serialize serializes the given data into a byte slice
+	Serialize(Claims) ([]byte, error)
+	// Deserialize deserializes the given byte slice into the given data
+	Deserialize([]byte) (Claims, error)
 }
