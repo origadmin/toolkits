ffc4182e5017fef8084e4f891032156ae21414b0
godcong <jumbycc@163.com>
2025-04-23 18:46:13 +0800

    feat(helpers): add Int64ToDuration function
    - Create time_duration.go file in toolkits/helpers directory
    - Implement Int64ToDuration function to convert int64 to time.Duration

---
+++ helpers/time_duration.go
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 OrigAdmin. All rights reserved.
+ */
+
+// Package helpers implements the functions, types, and interfaces for the module.
+package helpers
+
+import (
+	"time"
+)
+
+// Int64ToDuration converts an integer value to a time.Duration type.
+func Int64ToDuration(seconds int64) time.Duration {
+	return time.Duration(seconds) * 1e6
+}
