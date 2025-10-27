4e3954d2da606b2908c54c0e58b109c2fe9339de
godcong <jumbycc@163.com>
2025-10-27 14:28:21 +0800

    chore(crypto): Fix package comments in hash scheme module

--- crypto/hash/scheme/resolver.go
+++ crypto/hash/scheme/resolver.go
@@ -2,7 +2,7 @@
  * Copyright (c) 2024 OrigAdmin. All rights reserved.
  */
 
-// Package interfaces implements the functions, types, and interfaces for the module.
+// Package scheme implements the functions, types, and interfaces for the module.
 package scheme
 
 import (
